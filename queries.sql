71ed8b5492f319fedff7fc2e77d932ef4c798dd9
Yardley Gutierrez <yardleyg9@gmail.com>
2021-03-19 13:02:31 -0500

    Job view and ListController now have been modified to work with the database and display information properly. Completed writing out the database test queries.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,15 @@
 ## Part 1: Test it with SQL
+select *
+from INFORMATION_SCHEMA.COLUMNS
+where table_name = 'job';
 
 ## Part 2: Test it with SQL
+select employer.name from employer
+where employer.location = 'Saint Louis';
 
 ## Part 3: Test it with SQL
+Drop table job;
 
 ## Part 4: Test it with SQL
+select name, description from skill, job_skills
+where skill.id = job_skills.skills_id;
